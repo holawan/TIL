1b664121c23c3972de994f07600d2c088d2319dc
holawan <asdf134652@gmail.com>
2022-03-05 17:18:33 +0900

    0305

--- 0305/function.py
+++ 0305/function.py
@@ -1,3 +1,16 @@
-from math import * 
+from math import *
+import math
 
-print(tan(1/3))
+print(tan(3*pi/2))
+# tan(theta) = dy/dx
+# atan2(dy,dx) = theta (radian, pi/n의 형식)
+radian1 = atan2(55, 185) # theta가 60도인 직각삼각형. 밑변1, 높이 루트3
+radian2 = atan2( sqrt(3), -1) # theta가 120도인 삼각형. 밑변1, 높이 루트3
+radian3 = atan2( -sqrt(3), -1) # theta가 240도, 반바퀴 + 60도
+radian4 = atan2( -sqrt(3), 1) # theta가 300도, -60도
+print(radian1)
+print(degrees(radian1))
+print(degrees(radian2))
+print(degrees(radian3))
+print(degrees(radian4))
+print(degrees(acos(1/2)))
